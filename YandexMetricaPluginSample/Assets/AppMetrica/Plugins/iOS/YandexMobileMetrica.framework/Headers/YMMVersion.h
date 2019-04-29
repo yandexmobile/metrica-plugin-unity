fc8c7870e432edfa266d41493896f99ce1e3c765
Aliaksei Nestsiarovich <alex98@yandex-team.ru>
2019-04-29 13:39:29 +0300

    Release AppMetrica Unity Plugin 3.3.0

--- YandexMetricaPluginSample/Assets/AppMetrica/Plugins/iOS/YandexMobileMetrica.framework/Headers/YMMVersion.h
+++ YandexMetricaPluginSample/Assets/AppMetrica/Plugins/iOS/YandexMobileMetrica.framework/Headers/YMMVersion.h
@@ -1,6 +1,6 @@
 /*
  * Version for iOS
- * © 2012–2017 YANDEX
+ * © 2012–2019 YANDEX
  * You may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * https://yandex.com/legal/appmetrica_sdk_agreement/
@@ -10,9 +10,12 @@
 #define __YMM_VERSION_H__
 
 #define YMM_VERSION_MAJOR 3
-#define YMM_VERSION_MINOR 4
+#define YMM_VERSION_MINOR 6
 #define YMM_VERSION_PATCH 0
 
-#define YMM_BUILD_NUMBER 12950
+// This line is uncommented in pre-releases.
+// #define YMM_VERSION_PRERELEASE_ID "rc.1"
+
+#define YMM_BUILD_NUMBER 14209
 
 #endif // __YMM_VERSION_H__
--- YandexMetricaPluginSample/Assets/AppMetrica/YandexAppMetricaError.cs
+++ YandexMetricaPluginSample/Assets/AppMetrica/YandexAppMetricaError.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Version for Unity
+ * © 2018-2019 YANDEX
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * https://yandex.com/legal/appmetrica_sdk_agreement/
+ */
+
+using System;
 
 public enum YandexAppMetricaRequestDeviceIDError
 {
